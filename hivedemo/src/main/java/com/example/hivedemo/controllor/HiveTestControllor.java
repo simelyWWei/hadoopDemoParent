719e2950c69a455557e3176f71d38a7b9c8357f5
weiwei <weiw26@chinaunicom.cn>
2019-02-20 17:33:14 +0800

    update

--- hivedemo/src/main/java/com/example/hivedemo/controllor/HiveTestControllor.java
+++ hivedemo/src/main/java/com/example/hivedemo/controllor/HiveTestControllor.java
@@ -1,5 +1,6 @@
 package com.example.hivedemo.controllor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RestController
 public class HiveTestControllor {
 
@@ -35,4 +37,46 @@ public class HiveTestControllor {
         }
         return list;
     }
+
+    /**
+     * 查询Hive库中的某张数据表字段信息
+     */
+    @RequestMapping("/table/describe")
+    public List<String> describeTable(String tableName) throws SQLException {
+        List<String> list = new ArrayList<String>();
+        Statement statement = druidDataSource.getConnection().createStatement();
+        String sql = "describe " + tableName;
+        log.info("Running: " + sql);
+        ResultSet res = statement.executeQuery(sql);
+        while (res.next()) {
+            list.add(res.getString(1));
+        }
+        return list;
+    }
+
+    /**
+     * 查询指定tableName表中的数据
+     */
+    @RequestMapping("/table/select")
+    public List<String> selectFromTable(String tableName) throws SQLException {
+        Statement statement = druidDataSource.getConnection().createStatement();
+        String sql = "select * from " + tableName;
+        log.info("Running: " + sql);
+        ResultSet res = statement.executeQuery(sql);
+        List<String> list = new ArrayList<>();
+        int count = res.getMetaData().getColumnCount();
+        String str = null;
+        while (res.next()) {
+            str = "";
+            for (int i = 1; i < count; i++) {
+                str += res.getString(i) + " ";
+            }
+            str += res.getString(count);
+            log.info(str);
+            list.add(str);
+        }
+        return list;
+    }
+
+
 }
